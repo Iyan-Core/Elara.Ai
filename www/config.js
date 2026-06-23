ea2039e436465597aedb95597d43a76b89bc53d0
Iyan-Core <awianton2@gmail.com>
2026-06-24 06:53:28 +0700

    Create config.js

---
+++ www/config.js
@@ -0,0 +1,10 @@
+// www/config.js
+const CONFIG = {
+  // Pakai Gemini / Firebase (paling aman & gratis)
+  GEMINI_API_KEY: "AIza_Salin_Kunci_Kamu_Disini",
+  GEMINI_MODEL: "gemini-2.5-flash",
+
+  // Atau pakai OpenAI kalau mau
+  // OPENAI_API_KEY: "sk-proj-...",
+  // OPENAI_MODEL: "gpt-4o-mini"
+};
